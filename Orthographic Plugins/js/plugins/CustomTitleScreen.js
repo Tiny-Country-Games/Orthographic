e7cf11c046d7efa8783cb76e48c26be89b413f77
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2016-04-26 20:46:26 -0700

    Custom Title Screen Plugin

--- Orthographic Plugins/js/plugins/CustomTitleScreen.js
+++ Orthographic Plugins/js/plugins/CustomTitleScreen.js
@@ -4,8 +4,18 @@
  *
  * @author José Rodriguez-Rivas
  *
+ * @param Credit at Bottom
+ * @desc Set to "true" to draw the credit at the bottom of the title screen
+ * @default true
+ *
+ *
+ * @param Prefix Credit
+ * @desc Text drawn before name in credit.
+ * @default Created By
+ *
+ *
  * @param Developer Name
- * @desc Will write "Created by [name] at bottom of Title Screen"
+ * @desc Will write "[Prefix] [name]" at bottom of Title Screen
  * @default Foo Bar
  *
  * @help
@@ -15,9 +25,27 @@
 (function() {
 
   var _Scene_Title_createForeground = Scene_Title.prototype.createForeground;
-  var nameSprite;
+  var parameters = PluginManager.parameters("CustomTitleScreen");
+  var devName = String(parameters["Developer Name"]);
+  var credPre = String(parameters["Prefix Credit"]);
+  var drawCredit = String(parameters["Credit at Bottom"]) == "true";
+
   Scene_Title.prototype.createForeground = function() {
       _Scene_Title_createForeground.call(this);
+      this._devNameSprite = new Sprite(new Bitmap(Graphics.width, Graphics.height));
+      this.addChild(this._devNameSprite);
+      if(drawCredit) {
+        this.drawDeveloperName();
+      }
+  };
+
+  Scene_Title.prototype.drawDeveloperName = function() {
+    var x = 6;
+    var y = Graphics.height - 26;
+    var maxWidth = Graphics.width - x * 2;
+    var text = credPre + " " + devName;
+    this._devNameSprite.bitmap.fontSize = 24;
+    this._devNameSprite.bitmap.drawText(text, x, y, maxWidth, 12, 'left');
   };
 
   Scene_Title.prototype.drawGameTitle = function() {
